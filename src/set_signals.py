0d314586b4c78ababdfe69b4752a1eb418be0110
gerogiordano08 <85655339+gerogiordano08@users.noreply.github.com>
2025-05-09 19:13:43 -0300

    defined methods to get data from yfinance about a stock in the last n months and plot a chart with the data

--- src/set_signals.py
+++ src/set_signals.py
@@ -1,6 +1,9 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
 username = "username"
 stockname = ""
 time_take_account = 0
+
 def register_stockname():
     user_input = input(f"Well, hello {username}, which stock are you interested in? Just write the official abreviation. ").upper()
     verification_stock_name = input(f"You entered: {user_input}. Is that right?(y/n) ").lower()
@@ -13,8 +16,8 @@ def register_stockname():
 
 def get_time():
     try:
-        months = int(input("Enter how many months ago i should take into account.")) 
-        verification_months = input(f"You want to take into account a graph with the last {months} months. Is that right?").lower()
+        months = int(input("Enter how many months ago i should take into account. ")) 
+        verification_months = input(f"You want to take into account a graph with the last {months} months. Is that right? ").lower()
         if verification_months == 'y':
             return months
         else: 
@@ -24,12 +27,30 @@ def get_time():
         return 0
     
 
+def get_stock_data(s_name, s_time):
+    data = yf.download(s_name, period=s_time, interval="1d")
+    print(data)
+    return data.index, data["Low"]
+
+
+def plot_chart():
+    plt.plot(x, y, marker = '')
+    plt.xlabel("Day")
+    plt.ylabel("Price (pesos)")
+    plt.title("Lows chart")
+    plt.grid(True)
+    plt.show()
+
 if __name__ == "__main__":
-    username = input("What's your name?")
+    username = input("What's your name? ")
     
     while stockname == "":
         stockname = register_stockname()
     while time_take_account == 0:
         time_take_account = get_time()
-    print(stockname)
-    print(time_take_account)
+    
+    months = f"{time_take_account}mo"
+
+    x, y = get_stock_data(stockname, months)
+    plot_chart()
+
